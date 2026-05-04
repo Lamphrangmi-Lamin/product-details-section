2e267e5de48f9ab5c06c5f6027a035b74f81f2d0
Lamphrangmi <lamphrangmilamin46169@gmail.com>
2026-05-05 01:01:10 +0530

    feat: implement product data fetching with proper async and error handling

--- js/index.js
+++ js/index.js
@@ -1,2 +1,21 @@
-// Write custom JavaScript here.
-// You may ignore this file and delete if if JavaScript is not required for your challenge.
+const url = "https://www.greatfrontend.com/api/projects/challenges/e-commerce/products/voyager-hoodie";
+
+async function fetchData(url) {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  return response.json();
+}
+
+// console.log(fetchData(url));
+async function main() {
+  try {
+    const data = await fetchData(url);
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
