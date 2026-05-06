7f945283d5a7df9411142109a631add94ed36db1
Lamphrangmi <lamphrangmilamin46169@gmail.com>
2026-05-06 12:58:41 +0530

    feat: add renderStars helper function

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
-const url = "https://www.greatfrontend.com/api/projects/challenges/e-commerce/products/voyager-hoodie";
+const productName = document.getElementById("product-name");
+const productSpiel = document.getElementById("product-spiel");
+const reviewCount = document.getElementById("review-count");
+const ratingVal = document.getElementById("rating-value");
+const starsContainer = document.getElementById("stars-container");
 
-async function fetchData(url) {
-  const response = await fetch(url);
+async function fetchProductDetails(productId) {
+  const baseUrl =
+    "https://www.greatfrontend.com/api/projects/challenges/e-commerce/products/";
+  const response = await fetch(`${baseUrl}${productId}`);
   if (!response.ok) {
     throw new Error(`HTTP error! status: ${response.status}`);
   }
@@ -11,11 +17,43 @@ async function fetchData(url) {
 // console.log(fetchData(url));
 async function main() {
   try {
-    const data = await fetchData(url);
+    const data = await fetchProductDetails("elemental-sneakers");
     console.log(data);
+
+    productName.innerText = data.name;
+    productSpiel.innerText = data.description;
+    reviewCount.innerText = data.reviews;
+    ratingVal.innerText = Number(data.rating).toFixed(1);
+    starsContainer.innerHTML = renderStars(data.rating);
   } catch (error) {
     console.log(error);
   }
 }
 
 main();
+
+// Helper functions
+function renderStars(rating) {
+  const fullStars = Math.floor(rating);
+  const hasHalfStar = rating % 1 >= 0.5;
+  const emptyStars = 5 - fullStars - (hasHalfStar ? 1 : 0);
+
+  let starsHTML = "";
+
+  //Full stars
+  for (let i = 0; i < fullStars; i++) {
+    starsHTML += `<i class="ri-star-fill text-yellow-400"></i>`;
+  }
+
+  //Half star if needed
+  if (hasHalfStar) {
+    starsHTML += `<i class="ri-star-half-fill text-yellow-400"></i>`;
+  }
+
+  //Empty stars
+  for (let i = 0; i < emptyStars; i++) {
+    starsHTML += `<i class="ri-star-fill text-gray-200"></i>`;
+  }
+
+  return starsHTML;
+}
